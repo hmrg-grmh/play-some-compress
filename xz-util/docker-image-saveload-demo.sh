111b4759384592625d12941240d89a7162c627f6
Hm Renga Y <68635334+hmrg-grmh@users.noreply.github.com>
2021-05-06 21:02:39 +0800

    add by image(s).

--- xz-util/docker-image-saveload-demo.sh
+++ xz-util/docker-image-saveload-demo.sh
@@ -19,3 +19,18 @@ docker save docker.io/hello-world | xz -T0 --best > hello.tar.xz
 xz -d hello.tar.xz --stdout | docker load
 xz -d hello.tar.xz --stdout | docker load < /dev/stdin
 xz -d hello.tar.xz --stdout | docker load -i /dev/stdin
+
+
+# tar.xz bag by-image(s) in-parallel
+
+# save image(s)
+echo docker.io/rakudo-star:alpine | xargs -P0 -i{x} /usr/bin/bash -c 'docker pull '"'"{x}"'"' ; docker save '"'"{x}"'"' |  xz -T0 --best > "$(basename '"'"{x}"'"')".tar.xz'
+cat xxx | xargs -P0 -i{x} /usr/bin/bash -c 'docker pull '"'"{x}"'"' ; docker save '"'"{x}"'"' |  xz -T0 --best > "$(basename '"'"{x}"'"')".tar.xz'
+cat xxx | xargs -P0 -i{x} /usr/bin/bash -c 'docker pull '"'"{x}"'"' ; mkdir -p .../xxxx/"$(dirname '"'"{x}"'"')" ; docker save '"'"{x}"'"' |  xz -T0 --best > .../xxxx/'"'"{x}"'"'.tar.xz'
+
+# load image(s)
+cd .../xxxx ; ls *.tar.xz | xargs -P0 -i{x} /usr/bin/bash -c 'xz -d '"'"{x}"'"' --stdout | docker load'
+
+### images witten in xxx file and back bag will out to some dir ;
+### file name is always not include docker.io or swr.cn-east-2.myhuaweicloud.com/kuboard etc. ;
+### xargs -P <num> can setting the degree of parallelism , and 0 means full for the using cpu core ;
